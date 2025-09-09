84a3537245edf49b07f9a11afa89c9906f132de1
xin yi <limxinyi0180@gmail.com>
2025-09-09 21:31:07 +0800

    A-Packages: move classes into the duke package and fix imports

--- src/main/java/duke/Todo.java
+++ src/main/java/duke/Todo.java
@@ -1,16 +1,22 @@
 package duke;
 
 public class Todo extends Task {
+    public Todo(String description) {
+        super(description, false);
+    }
+
     public Todo(String description, boolean isDone) {
         super(description, isDone);
     }
-    @Override public String serialize() {
-        return String.format("T | %d | %s", isDone ? 1 : 0, description);
+
+    @Override
+    public String serialize() {
+        return String.format("T | %d | %s", isDone() ? 1 : 0, getDescription());
     }
+
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
-
 }
 
